8e6f6c95dbbbc004eddb2c892025af9fb858391d
chiarabbb <chiarabarberi@yahoo.it>
2021-10-13 00:27:10 +0200

    newVersion

--- sketch.js
+++ sketch.js
@@ -1,13 +1,39 @@
-function preload() {
-  // put preload code here
-}
+let spaceX = 20,
+  spaceY = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  noStroke();
 }
 
+let seed = 0;
+let increment = 0.010;
+
 function draw() {
-  // put drawing code here
-  background("red");
+  background("white");
+
+  const _y = noise(seed) * height
+  const _x = noise(seed + 400) * width
+
+  seed += increment
+
+  //horizontal row
+  for (let x = 20; x < width; x += spaceX) {
+    //vertical column
+    for (let y = 20; y < height; y += spaceY) {
+      //figure out distance from the mouse
+      let d = dist(_x, _y, x, y);
+      if (d < 100) {
+        fill(0, 32, 255, 100);
+        //adjust diam based on distance
+        diam = map(d, 0, 100, 50, 1);
+      } else {
+        fill("pink");
+        diam = 10
+      }
+
+
+      ellipse(x, y, diam);
+    }
+  }
 }
